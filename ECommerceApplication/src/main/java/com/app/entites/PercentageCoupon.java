e7dce98d850ba1070c3eae1c21bf94a6501ab27f
yudiptr <yudiputrasabri123@gmail.com>
2025-02-21 14:46:12 +0700

    chore: typhography

--- ECommerceApplication/src/main/java/com/app/entites/PercentageCoupon.java
+++ ECommerceApplication/src/main/java/com/app/entites/PercentageCoupon.java
@@ -1,25 +1,25 @@
-    package com.app.entites;
+package com.app.entites;
 
-    import jakarta.persistence.*;
-    import lombok.*;
+import jakarta.persistence.*;
+import lombok.*;
 
-    @Entity
-    @Table(name = "percentage_coupons")
-    @Setter
-    @NoArgsConstructor
-    @AllArgsConstructor
-    public class PercentageCoupon extends Coupon {
+@Entity
+@Table(name = "percentage_coupons")
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PercentageCoupon extends Coupon {
 
-        @Column(nullable = false)
-        private double discountPercentage; 
+    @Column(nullable = false)
+    private double discountPercentage; 
 
-        @Override
-        public double calculateDiscount(double originalPrice) {
-            return originalPrice * discountPercentage / 100;
-        }
-
-        @Override
-        public double getDiscountAmount() {
-            return discountPercentage;
-        }
+    @Override
+    public double calculateDiscount(double originalPrice) {
+        return originalPrice * discountPercentage / 100;
     }
+
+    @Override
+    public double getDiscountAmount() {
+        return discountPercentage;
+    }
+}
